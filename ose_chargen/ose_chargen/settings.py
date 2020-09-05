b09fb7712f8f548d7bc087815d48b1bde13a8e04
jamalainm <robot@inman.xyz>
2020-09-04 22:20:24 -0500

    Starting app

--- ose_chargen/ose_chargen/settings.py
+++ ose_chargen/ose_chargen/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^08be^pivomjdrf_*#qx%jd8@^4pf0j(eile0l+y8+3jfn3)sm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'ose_chargen.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
